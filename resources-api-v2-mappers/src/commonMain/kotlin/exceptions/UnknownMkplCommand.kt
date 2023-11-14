d134a44085259e34b6310ad244a4a6260e64936e
Zuzichev <zuzichev@mail.ru>
2023-11-14 17:06:07 +0500

    выпилил маркетплейс, добавил sql

--- resources-api-v2-mappers/src/commonMain/kotlin/exceptions/UnknownMkplCommand.kt
+++
@@ -1,5 +0,0 @@
-package ru.otus.otuskotlin.marketplace.mappers.v2.exceptions
-
-import ru.otus.otuskotlin.marketplace.common.models.ResourcesCommand
-
-class UnknownMkplCommand(command: ResourcesCommand) : Throwable("Wrong command $command at mapping toTransport stage")
--- resources-app-common/src/commonMain/kotlin/IResorcesAppSettings.kt
+++
@@ -1,9 +0,0 @@
-package ru.otus.otuskotlin.marketplace.app.common
-
-import ru.otus.otuskotlin.marketplace.biz.ResourcesProcessor
-import ru.otus.otuskotlin.marketplace.common.ResourcesCorSettings
-
-interface IResorcesAppSettings {
-    val processor: ResourcesProcessor
-    val corSettings: ResourcesCorSettings
-}
